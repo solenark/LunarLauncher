53ddf8d09dae85c40ff9d5d365bbb48a6ce615f9
Lucas Caldeira <l.caldeira.m@gmail.com>
2021-02-01 11:14:11 -0300

    archives changes
    title menu added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
 
 const Hello = () => {
   return (
     <div>
       <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
+        <div className="Menu-title">
+          <button type="button" className="Netplay">
+            Netplay
           </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
+          <button type="button" className="Offline">
+            Offline
           </button>
-        </a>
+          <button type="button" className="Spectate">
+            Spectate
+          </button>
+          <button type="button" className="Replays">
+            Replays
+          </button>
+          <button type="button" className="Controls">
+            Controls
+          </button>
+          <button type="button" className="Settings">
+            Settings
+          </button>
+        </div>
       </div>
     </div>
   );
